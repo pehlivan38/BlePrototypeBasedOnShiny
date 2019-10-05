3feea010bc9e4e04a7176d8b876801d9c6634120
Serdar PEHLIVANOGLU <Serdar PEHLIVANOGLU@DESKTOP-PI2LTI6>
2019-10-05 03:43:35 +0300

    android project added

--- BLEPrototype/BLEPrototype.Android/MainActivity.cs
+++ BLEPrototype/BLEPrototype.Android/MainActivity.cs
@@ -1,16 +1,25 @@
 ﻿using System;
-
+using Shiny;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Content;
 
 namespace BLEPrototype.Droid
 {
-    [Activity(Label = "BLEPrototype", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(
+        Label = "BLEPrototype", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,8 +27,27 @@ namespace BLEPrototype.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+
+            Forms.SetFlags("CollectionView_Experimental");
+            Forms.Init(this, savedInstanceState);
+            
+            //Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            this.LoadApplication(new App());
+
+            //Shiny.Notifications.NotificationManager.TryProcessIntent(this.Intent);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            //Shiny.Notifications.NotificationManager.TryProcessIntent(intent);
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            AndroidShinyHost.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
---
+++ BLEPrototype/BLEPrototype.Android/MainApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BLEPrototype.Startup;
+using Shiny;
+
+namespace BLEPrototype.Droid
+{
+    [Application]
+    public class MainApplication : ShinyAndroidApplication<BLEStartup>
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+    }
+}
